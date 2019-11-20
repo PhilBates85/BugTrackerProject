7fe628cb45d0e58bb49e84a487eb4892c9788cf6
Phillip Bates <phillipmbates85@gmail.com>
2019-11-20 14:24:17 -0600

    11.20.19

--- django/prototype/main/urls.py
+++ django/prototype/main/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'main'  # here for namespacing of urls.
 
 urlpatterns = [
-  path("", views.homepage, name="homepage"),
-  path("register/", views.register, name="register"),
+    path("", views.homepage, name="homepage"),
+    path("register/", views.register, name="register"),
+    path("logout/", views.logout_request, name="logout"),
+    path("login", views.login_request, name="login"),
 ]
