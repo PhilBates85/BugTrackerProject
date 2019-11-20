a7203208113de89bf3f89f225768939ec023d81a
Phillip Bates <phillipmbates85@gmail.com>
2019-11-19 18:32:08 -0600

    registerpage

--- django/prototype/main/urls.py
+++ django/prototype/main/urls.py
@@ -1,8 +1,10 @@
-from . import views
 from django.urls import path
+from . import views
 
-app_name = "main"
+
+app_name = 'main'  # here for namespacing of urls.
 
 urlpatterns = [
-    path("", views.homepage, name="homepage"),
+  path("", views.homepage, name="homepage"),
+  path("register/", views.register, name="register"),
 ]
